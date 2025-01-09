438592f95944fc4a900425c2aa46cd4d9346a575
der1x <130235333+Mark4353@users.noreply.github.com>
2025-01-09 15:20:45 +0200

    football

---
+++ src/js/football.js
@@ -0,0 +1,15 @@
+const gameField = document.getElementById("game-field");
+const ball = document.getElementById("ball");
+const gates = document.getElementById("gates")
+
+gameField.addEventListener('mousemove', (event) => {
+    const gameFieldRect = gameField.getBoundingClientRect();
+
+    const x = event.clientX - gameFieldRect.left;
+    const y = event.clientY - gameFieldRect.top;
+    const maxX = Math.max(0, Math.min(x, gameFieldRect.width - ball.offsetWidth));
+    const maxY = Math.max(0, Math.min(y, gameFieldRect.height - ball.offsetHeight));
+
+    ball.style.left = `${maxX}px`;
+    ball.style.top = `${maxY}px`;
+});
